11689a6a87536b025a4f580f4a718f58b134b1ba
Yhaliff <whyis2cool@gmail.com>
2026-09-20 23:10:54 -0600

    SystemsBase.gd

---
+++ Scripts/Systems/SystemsBase.gd
@@ -0,0 +1,22 @@
+@abstract class_name SystemBase extends Node2D
+
+enum SystemResults
+{
+	NO_ERROR = 1,
+	GENERAL_ERROR = 0,
+	COULD_NOT_GET_RESOURCES_ERROR = -1,
+}
+
+enum SystemTypes {}
+
+
+@abstract func startup() -> SystemResults ;
+
+
+@abstract func shutdown() -> SystemResults ;
+
+
+@abstract func debug_string() -> String;
+
+
+@abstract func debug_print() -> void;
