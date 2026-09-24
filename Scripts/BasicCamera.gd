3f266b83187328b8de727d75d8db1d9df19fd03a
Yhaliff <whyis2cool@gmail.com>
2026-09-24 12:32:17 -0600

    Add state to BasicCamera.gd

--- Scripts/BasicCamera.gd
+++ Scripts/BasicCamera.gd
@@ -1,7 +1,12 @@
 class_name BasicCamera extends Camera2D
 
+var state: Enums.GameState
+
 
 func _ready() -> void:
+	var state_man := SystemsLocator.get_state_manager()
+	state = state_man.get_current_state()
+	state_man.state_change.connect(cb_state_change)
 	pass # Replace with function body.
 
 
@@ -23,11 +28,17 @@ func handle_mouse_wheel_input(event: InputEventMouse) -> void:
 		self.zoom += Vector2(0.1, 0.1) * (-1)
 	return
 
-func handle_mouse_movement(event : InputEventMouseMotion) -> void:
+
+func handle_mouse_movement(event: InputEventMouseMotion) -> void:
 	return
 
+
 ## TODO: implement this 
-func handle_pan_gesture_input(event : InputEventPanGesture) -> void:
+func handle_pan_gesture_input(event: InputEventPanGesture) -> void:
 	var _delta := event.delta
 	return
 #endregion
+
+
+func cb_state_change(new_state: Enums.GameState) -> void:
+	state = new_state
