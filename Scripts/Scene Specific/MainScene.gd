a707a0fa1f77bf2b69fd22d5e9695b954ba0916f
Yhaliff <whyis2cool@gmail.com>
2026-09-24 12:20:48 -0600

    Main scene start up the service locator.

--- Scripts/Scene Specific/MainScene.gd
+++ Scripts/Scene Specific/MainScene.gd
@@ -2,7 +2,21 @@ extends Node
 
 var camera : BasicCamera = null
 var state : Enums.GameState = Enums.GameState.idle
+var state_man : StateManager = null
+
+func _enter_tree() -> void:
+	SystemsLocator.startup()
+
 func _ready() -> void:
+	state_man = SystemsLocator.get_state_manager()
+	state_man.state_change.connect(_on_state_change)
+	
 	camera = BasicCamera.new()
 	add_child(camera)
 	pass # Replace with function body.
+
+func _exit_tree() -> void:
+	SystemsLocator.shutdown()
+
+func _on_state_change(new_state:Enums.GameState) -> void:
+	state = new_state
